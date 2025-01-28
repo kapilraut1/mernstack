6277ff46dadacc317ae9d2c9cca5298819f730ca
Kapil Raut <kapilraut026@gmail.com>
2025-01-28 21:28:34 +0545

    day2.3

--- express2.js/app.js
+++ express2.js/app.js
@@ -3,18 +3,19 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.post("/", (req, res) => {
-    res.send("Hello World Post");
-    console.log("Checking");
-  });
-  app.get("/index", (req, res) => {
+
+ // app.get("/index", (req, res) => {
+//res.sendFile('./templates/app.html', {root: __dirname});
+  //});
+
+app.get("/index1", (req, res) => {
     res.sendFile('./templates/app.html', {root: __dirname});
-  });
+res.download|('./templates/hello.txt');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
