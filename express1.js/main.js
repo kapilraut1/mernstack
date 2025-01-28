87b1e419d8f4ef1eb67cbb401e7c903b963947d5
Kapil Raut <kapilraut026@gmail.com>
2025-01-28 08:46:16 +0545

    day2.1

--- express1.js/main.js
+++ express1.js/main.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//using slug
+app.get('/hello/:slug', (req, res) => {
+  res.send(`Hello ${req.params.slug}`)
+  console.log(req.params);
+  console.log(req.query);
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
