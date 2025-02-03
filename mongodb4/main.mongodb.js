48532ddeaf0fa9d2e972786034f14d3e7a33dbcf
Kapil Raut <kapilraut026@gmail.com>
2025-02-03 22:42:01 +0545

    crud

--- mongodb4/main.mongodb.js
+++ mongodb4/main.mongodb.js
@@ -1,5 +1,5 @@
 use("day6")
-
+/*
 db.try.insertOne({
     name: "Kaps",
     age: 20
@@ -33,3 +33,53 @@ db.try.insertMany([{
     name: "Kaps",
     age: 20
 }])
+
+db.try.findOne({name: "Kaps"}, {age: 1})
+
+db.prac.insertMany ([ {
+    title: "Post Title 2",
+    body: "Body of post.",
+    category: "Event",
+    likes: 2,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 3",
+    body: "Body of post.",
+    category: "Technology",
+    likes: 3,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 4",
+    body: "Body of post.",
+    category: "Event",
+    likes: 4,
+    tags: ["news", "events"],
+    date: Date()
+  }])
+  
+
+ // db.prac.findOne()
+ //db.prac.find({}, {title:0})
+
+ //db.prac.updateOne({}, {$set: {likes: 4}})
+ db.prac.updateOne( 
+    { title: "Post Title 5" }, 
+    {
+      $set: 
+        {
+          title: "Post Title 5",
+          body: "Body of post.",
+          category: "Event",
+          likes: 5,
+          tags: ["news", "events"],
+          date: Date()
+        }
+    }, 
+    { upsert: true }
+  )
+ */
+  db.prac.updateMany({}, { $inc: { likes: 1 } })
